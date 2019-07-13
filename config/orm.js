e6733187f46c078a5690eff12c1ee409b2bf355f
unknown <hannahuseva@gmail.com>
2019-07-13 18:51:41 -0500

    Seems functional

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,10 @@
 var connection = require("../config/connection.js")
 function printQuestionMarks(num) {
-  var arr = [];
-
+  var arr = []
   for (var i = 0; i < num; i++) {
-    arr.push("?");
+    arr.push("?")
   }
-
-  return arr.toString();
+  return arr.toString()
 }
 
 function objToSql(ob) {
@@ -23,56 +21,46 @@ function objToSql(ob) {
   return arr.toString()
 }
 
-
 var orm = {
-    selectAll: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";"
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err
-          }
-          cb(result)
-        })
-    },
-    insertOne: function(table, cols, vals, cb) {
-      var queryString = "INSERT INTO " + table;
-  
-      queryString += " (";
-      queryString += cols.toString();
-      queryString += ") ";
-      queryString += "VALUES (";
-      queryString += printQuestionMarks(vals.length);
-      queryString += ") ";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, vals, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
-
-    },
-    updateOne: function(table, objColVals, condition, cb) {
-      var queryString = "UPDATE " + table;
-  
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
-  
-      console.log(queryString);
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
-
-    }
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";"
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result)
+    })
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table
+    queryString += " ("
+    queryString += cols.toString()
+    queryString += ") "
+    queryString += "VALUES ("
+    queryString += printQuestionMarks(vals.length)
+    queryString += ") "
+    console.log(queryString)
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result)
+    })
+  },
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table
+    queryString += " SET ";
+    queryString += objToSql(objColVals)
+    queryString += " WHERE "
+    queryString += condition
+    console.log(queryString)
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result)
+    })
+  }
 }
 
 module.exports = orm
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,37 +3,36 @@ var burger = require("../models/burger.js")
 var router = express.Router()
 
 router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-      var hbsObject = {
-        burgers: data
-      }
-      res.render("index", hbsObject)
-    })
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    }
+    res.render("index", hbsObject)
+  })
 })
 
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
-      "burger_name", "devoured:"
-    ], [
-      req.body.burger_name, false
-    ], function(result) {
-      console.log("working")
-      res.json(result)
-    })
+  burger.insertOne([
+    "burger_name", "devoured"
+  ],
+  [
+    req.body.burger_name, false
+  ], function(result) {
+    res.json(result)
+  })
 })
   
 router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id
-    console.log("condition", condition)
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        return res.status(404).end()
-      } else {
-        res.status(200).end()
-      }
-    })
+  var condition = "id = " + req.params.id
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end()
+    } else {
+      res.status(200).end()
+    }
+  })
 })
 
 module.exports = router
